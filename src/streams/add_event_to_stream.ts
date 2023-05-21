1334d05997cad7c29d956ae38d1c539b3f9e676d
Andre Ohuti <ohuti@hotmail.com>
2023-05-21 20:21:00 -0300

    streams demo completed

---
+++ src/streams/add_event_to_stream.ts
@@ -0,0 +1,38 @@
+import * as amqp from 'amqplib'
+
+import { sleep } from '../utils'
+
+(async () => {
+    const user = {
+        name: 'André',
+        email: 'andre@test.com',
+        createdAt: new Date().toISOString(),
+        updatedAt: null
+    }
+    const connection = await amqp.connect('amqp://localhost')
+    const channel = await connection.createChannel()
+    await channel.assertQueue('my-stream', { durable: true, exclusive: false, autoDelete: false, arguments: {
+        'x-queue-type': 'stream',
+        'x-max-length-bytes': 20_000_000_000,
+        'x-stream-max-segment-size-bytes': 100_000_000
+    }})
+
+    const userCreated = {
+        event: 'UserCreated',
+        ocurredAt: user.createdAt,
+        data: user
+    }
+    channel.sendToQueue('my-stream', Buffer.from(JSON.stringify(userCreated)))
+
+    user.name = 'André Ohuti'
+    user.updatedAt = new Date().toISOString()
+    const userUpdated = {
+        event: 'UserUpdated',
+        createdAt: user.updatedAt,
+        data: user
+    }
+    channel.sendToQueue('my-stream', Buffer.from(JSON.stringify(userUpdated)))
+
+    await sleep(2000)
+    await connection.close()
+})()
---
+++ src/streams/read_events.ts
@@ -0,0 +1,20 @@
+import * as amqp from 'amqplib'
+
+(async () => {
+    const connection = await amqp.connect('amqp://localhost')
+    const channel = await connection.createChannel()
+    await channel.assertQueue('my-stream', { durable: true, exclusive: false, autoDelete: false, arguments: {
+        'x-queue-type': 'stream',
+        'x-max-length-bytes': 20_000_000_000,
+        'x-stream-max-segment-size-bytes': 100_000_000
+    }})
+    await channel.prefetch(1)
+    await channel.consume('my-stream', (message) => {
+        const event = JSON.parse(message.content.toString())
+        console.info(`[x] Event ${event.event} received at ${event.ocurredAt}.`)
+        console.info(`  [-] User name: ${event.data.name}.`)
+        console.info(`  [-] User e-mail: ${event.data.email}.`)
+        channel.ack(message)
+    }, { arguments: { 'x-stream-offset': 'first' }})
+    console.info('[x] Waiting for logs. To exit press CTRL+C...')
+})()
